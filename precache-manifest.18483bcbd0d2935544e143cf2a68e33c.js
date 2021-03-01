52c0386c499e55d84f943d7ce719f7ccb5d60e30
jisha-jacob <jisha-jacob@users.noreply.github.com>
2021-02-28 22:45:02 -0500

    Updates

---
+++ precache-manifest.18483bcbd0d2935544e143cf2a68e33c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "268a403d8bc6726274be",
+    "url": "/productivity-app/static/js/main.ff9d355a.chunk.js"
+  },
+  {
+    "revision": "70148f307d8f02656529",
+    "url": "/productivity-app/static/js/runtime~main.5ebbdb93.js"
+  },
+  {
+    "revision": "fd262798d3de47d33a20",
+    "url": "/productivity-app/static/js/2.d7b74634.chunk.js"
+  },
+  {
+    "revision": "08c565a2c662be2b114d4e6116dc9f80",
+    "url": "/productivity-app/index.html"
+  }
+];
